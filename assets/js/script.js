4058ba294cf567da51cfe06e1b1f1cee04f37860
Crape Myrtle <safiatraore1@gmail.com>
2022-06-28 18:46:39 -0400

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,45 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
+    
     event.preventDefault();
     
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    //create list item
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
+    //package data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    //create div to hold task info and add to list item
-    var taskInfoE1 = document.createElement("div");
-    taskInfoE1.className = "task-info";
-
-    //add HTML content to div
-    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemE1.appendChild(taskInfoE1);
-
-    //add entire list item to list
-    tasksToDoE1.appendChild(listItemE1);
+    //send it as an argument to createTaskE1
+    createTaskE1(taskDataObj);
 
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+var createTaskE1 = function(taskDataObj) {
+  //create list item
+  var listItemE1 = document.createElement("li");
+  listItemE1.className = "task-item";
+
+  //create div to hold task info and add to list item
+  var taskInfoE1 = document.createElement("div");
+  taskInfoE1.className = "task-info";
+
+  //add HTML content to div
+  taskInfoE1.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3><span class='task-type'>" +
+    taskDataObj.type +
+    "</span>";
+
+  listItemE1.appendChild(taskInfoE1);
+
+  //add entire list item to list
+  tasksToDoE1.appendChild(listItemE1);
+}
+
+formE1.addEventListener("submit", taskFormHandler);
